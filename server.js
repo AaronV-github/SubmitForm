2693f5ca53639ae6f3b5dcb126d834d06ab0c74d
Aaron Vega <aaronjv96@yahoo.com>
2022-06-22 13:53:48 -0500

    1st commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.post('/name-submit', function (req, res) {
+    let name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(`Hello ${name}`);
+});
+
+var server = app.listen(port, function () {
+    console.log('Server is running..');
+});
